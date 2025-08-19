ce842df62590297d304ce1bf05ef9f4cb4e535e1
Sribas Chowdhury <sribas.chowdhury888@gmail.com>
2025-08-19 14:55:51 +0530

    Create Module Scoring.R

---
+++ Module Scoring.R
@@ -0,0 +1,27 @@
+library(tidyverse)
+library(RColorBrewer)
+library(Seurat)
+library(SeuratData)
+library(future)
+
+data <- readRDS("......../data_motifenriched.rds")
+
+module_scores <- read.csv(".../Module_scoring.csv")
+melanocytic_genes <- na.omit(module_scores$Melanocytic.lineage.score)
+glial_genes       <- na.omit(module_scores$Glial.Neuronal.lineage.score)
+muscle_genes      <- na.omit(module_scores$Muscle.lineage.score)
+
+# Add module scores
+data <- AddModuleScore(data, features = list(melanocytic_genes), name = "Melanocytic_score")
+data <- AddModuleScore(data, features = list(glial_genes), name = "Neural_score")
+data <- AddModuleScore(data, features = list(muscle_genes), name = "Muscle_score")
+
+#Represent using Module Score
+FeaturePlot(data, features = "Melanocytic_score1", reduction = "umap.rna")+
+  scale_colour_gradientn(colours = rev(brewer.pal(n = 11, name = "RdBu")))
+FeaturePlot(data, features = "Neural_score1", reduction = "umap.rna")+
+  scale_colour_gradientn(colours = rev(brewer.pal(n = 11, name = "RdBu")))
+FeaturePlot(data, features = "Muscle_score1", reduction = "umap.rna")+
+  scale_colour_gradientn(colours = rev(brewer.pal(n = 11, name = "RdBu")))
+
+saveRDS(data, file = "...../data_module.rds")
